99d64763f5d72ea3cb4e829dc89d7842b3245f6f
Luisa <104804096+luisacoutinho06@users.noreply.github.com>
2022-11-12 20:00:22 -0300

    Add files via upload

---
+++ src/Luisa_Diversão e interatividade/interatividade_e_diversao/coletascript.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    var botao = $('.bt1');
+    var dropDown = $('.ul-redes');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+        })})
+
