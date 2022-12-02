d7891b515ece6ed49bca2cc6951136de02e9d7d7
Thiago <115198478+ThiagoOliveiraQ@users.noreply.github.com>
2022-12-01 21:12:29 -0300

    Delete src directory

--- src/Jadir-Faq/$(document)..ready(function() {.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    var botao = $('.bt1');
-    var dropDown = $('.ul-redes');    
-   
-       botao.on('click', function(event){
-           dropDown.stop(true,true).slideToggle();
-           event.stopPropagation();
-        })})
